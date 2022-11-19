4f8ca8c77e35b0713a350af53e26c67aabbe0ac0
Ambika Sahu <90752801+AmbikaSahu2577@users.noreply.github.com>
2022-11-19 13:07:08 +0530

    Add files via upload

---
+++ Type_Conversion.java
@@ -0,0 +1,13 @@
+class Type_Conversion
+{
+	public static void main(String[] args) 
+	{
+	  String str = "java";
+	  int num = 10;
+	  String str1 = String.valueOf(num);
+	  System.out.println(str1);
+	  char ch[] = str.toCharArray();
+	  System.out.println(ch);	
+	}
+	
+}
